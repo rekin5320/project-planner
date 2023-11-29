ac6de8e6eb38bd808b5c1862be0b77d5f4862923
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2023-11-29 12:11:00 +0100

    Not working JPA implementation

--- src/main/java/pw/pap/Main.java
+++ src/main/java/pw/pap/Main.java
@@ -1,11 +1,11 @@
 package pw.pap;
 
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
 
 
 public class Main {
