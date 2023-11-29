1eaba15af202d0ce46531bcec6c17fd4c5bd91b0
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2023-11-29 11:49:44 +0100

    Not working JPA implementation

--- src/main/java/pw/pap/Main.java
+++ src/main/java/pw/pap/Main.java
@@ -1,7 +1,135 @@
 package pw.pap;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+
 public class Main {
+    private static final EntityManagerFactory
+        ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("PW.PAP");
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        deleteUsers();
+        createUsers();
+        updateUser();
+        deleteUser();
+
+        List<User> users = readAll();
+        if (users != null) {
+            for (User u : users)
+                System.out.println(u);
+        }
+
+        ENTITY_MANAGER_FACTORY.close();
     }
+
+    public static void createUsers() {
+        EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = manager.getTransaction();
+            transaction.begin();
+
+            User jeremy = new User();
+            jeremy.setId(1);
+            jeremy.setName("Jeremy");
+            manager.persist(jeremy);
+
+            User james = new User();
+            james.setId(2);
+            james.setName("James");
+            manager.persist(james);
+
+            User richard = new User();
+            james.setId(3);
+            james.setName("Richard");
+            manager.persist(richard);
+
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null)
+                transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            manager.close();
+        }
+    }
+
+    public static List<User> readAll() {
+        List<User> result = null;
+        EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = manager.getTransaction();
+
+            transaction.begin();
+            result = manager.createQuery("SELECT u FROM User u", User.class).getResultList();
+
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null)
+                transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            manager.close();
+        }
+        return result;
+    }
+
+    public static void deleteUser() {
+        EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = manager.getTransaction();
+            transaction.begin();
+            User u = manager.find(User.class, 3);
+            manager.remove(u);
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null)
+                transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            manager.close();
+        }
+    }
+
+    public static void deleteUsers() {
+        EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = manager.getTransaction();
+            transaction.begin();
+            Query query = manager.createQuery("DELETE FROM User u");
+            int deleteRecords = query.executeUpdate();
+            transaction.commit();
+            System.out.println("All records were deleted.");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            manager.close();
+        }
+    }
+
+    public static void updateUser() {
+        EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = manager.getTransaction();
+            transaction.begin();
+            User u = manager.find(User.class, 2);
+            u.setName("JAMES");
+            manager.persist(u);
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null)
+                transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            manager.close();
+            }
+        }
 }
---
+++ src/main/java/pw/pap/User.java
@@ -0,0 +1,50 @@
+package pw.pap;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "User")
+public class User implements Serializable {
+    @Id
+    @Column (name = "UserID", unique = true)
+    private int id;
+
+    @Column (name = "Name", nullable = false)
+    private String name;
+
+    public int getId() {return id;}
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {return name;}
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return id + "\t" + name;
+    }
+}
+
+/*
+-- Init database:
+create database papdb;
+create user myuser@'%' identified by 'passw'
+GRANT ALL PRIVILEGES ON papdb.* TO 'myuser'@'%';
+
+use papdb;
+create table tasks_tmp (
+    task_id INT primary key,
+    name varchar(99) not null
+);
+show tables;
+insert into tasks_tmp values (1, 'Pierwsze zadanie');
+insert into tasks_tmp values (2, 'Drugie zadanie');
+*/
