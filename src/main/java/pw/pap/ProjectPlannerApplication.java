a521a0b45bb63490ac76af08e353d032a29671d0
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2023-12-02 16:18:19 +0100

    Refactored file structure, so it uses spring boot.

---
+++ src/main/java/pw/pap/ProjectPlannerApplication.java
@@ -0,0 +1,12 @@
+package pw.pap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPlannerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pw/pap/ProjectPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.pap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
