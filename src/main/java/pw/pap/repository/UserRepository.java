6f2a4773fa9d2b463debbba4afd59aebebce10b7
User <kubapolip0@gmai.com>
2023-12-03 20:55:56 +0100

    Corrected imports

--- src/main/java/pw/pap/repository/UserRepository.java
+++ src/main/java/pw/pap/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package pw.pap.repository;
 
-import pw.pap.database.User;
+import pw.pap.api.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
