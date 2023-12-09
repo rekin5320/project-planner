d8aef972c9a543a5ae0ae9cc618c632e77cb9c4e
Michał Machnikowski <01178559@pw.edu.pl>
2023-12-09 03:21:59 +0100

    Reformatted files

--- src/my-react-frontend/src/App.js
+++ src/my-react-frontend/src/App.js
@@ -1,13 +1,13 @@
-import './tailwind.css';
-import React from 'react';
-import UserManagementComponent from './components/UserManagementComponent';
+import "./tailwind.css";
+import React from "react";
+import UserManagementComponent from "./components/UserManagementComponent";
 
 function App() {
-  return (
-    <div className="bg-custom-pink min-h-screen">
-      <UserManagementComponent />
-    </div>
-  );
+    return (
+        <div className="bg-custom-pink min-h-screen">
+            <UserManagementComponent/>
+        </div>
+    );
 }
 
 export default App;
