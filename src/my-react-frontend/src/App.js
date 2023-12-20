253ba974388efe07e9026e7737b6d612d470819e
Michał Machnikowski <01178559@pw.edu.pl>
2023-12-20 10:29:49 +0100

    Wrap elements in flexbox

--- src/my-react-frontend/src/App.js
+++ src/my-react-frontend/src/App.js
@@ -5,7 +5,7 @@ import ProjectManagementComponent from "./components/ProjectManagementComponent"
 
 function App() {
     return (
-        <div className="flex justify-evenly bg-custom-pink min-h-screen">
+        <div className="flex flex-wrap justify-evenly bg-custom-pink min-h-screen">
             <UserManagementComponent/>
             <ProjectManagementComponent/>
         </div>
