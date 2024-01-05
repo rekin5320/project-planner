32b6da8470d85d689e93149ea4bbcc5eb2c11098
Jakub <01178575@pw.edu.pl>
2024-01-05 11:49:53 +0100

    Corrected imports in TaskController

--- src/main/java/pw/pap/api/controller/TaskController.java
+++ src/main/java/pw/pap/api/controller/TaskController.java
@@ -5,9 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import pw.pap.api.model.Task;
-import pw.pap.api.model.User;
-import pw.pap.api.model.Project;
+import pw.pap.model.Task;
+import pw.pap.model.User;
+import pw.pap.model.Project;
 import pw.pap.service.TaskService;
 
 import java.time.LocalDateTime;
