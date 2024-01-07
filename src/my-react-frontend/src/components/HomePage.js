63b52381fdec2901e8162d04b3a480cfee1f2885
Michał Machnikowski <01178559@pw.edu.pl>
2024-01-07 20:37:35 +0100

    Added `UserManagementComponent` to home page

--- src/my-react-frontend/src/components/HomePage.js
+++ src/my-react-frontend/src/components/HomePage.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import UserManagementComponent from "./UserManagementComponent.js";
 import ProjectManagementComponent from "./ProjectManagementComponent";
 
 function HomePage({ userCredentials }) {
     return (
         <div>
-            <ProjectManagementComponent> </ProjectManagementComponent>
-            <p>Username: {userCredentials.username}</p>
-            <p>Password: {userCredentials.password}</p> {/* Be cautious about displaying passwords */}
+            <div className="min-h-screen bg-custom-background">
+                <p>Username: {userCredentials.username}</p>
+                <p>Password: {userCredentials.password}</p> {/* Be cautious about displaying passwords */}
+
+                <div className="flex flex-wrap justify-evenly content-evenly">
+                    <UserManagementComponent/>
+                    <ProjectManagementComponent/>
+                </div>
+            </div>
         </div>
     );
 }
