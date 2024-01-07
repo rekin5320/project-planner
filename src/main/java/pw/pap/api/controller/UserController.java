0b53bde36d3d8f4ddbdd3b35659b7ea260ca1a2a
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2024-01-07 21:50:50 +0100

    Updated controllers for login and register

--- src/main/java/pw/pap/api/controller/UserController.java
+++ src/main/java/pw/pap/api/controller/UserController.java
@@ -1,6 +1,9 @@
 package pw.pap.api.controller;
 
 import java.util.List;
+
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,9 +21,26 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/all")
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
+    @PostMapping("/login")
+    public ResponseEntity<User> loginUser(@RequestBody UserAndPasswordDTO userAndPasswordDTO) {
+        try {
+            User user = userService.login(userAndPasswordDTO.getName(), userAndPasswordDTO.getPassword());
+            return ResponseEntity.ok(user);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody UserAndPasswordDTO userAndPasswordDTO) {
+        try {
+            User user = userService.register(userAndPasswordDTO.getName(), userAndPasswordDTO.getPassword());
+            return ResponseEntity.status(HttpStatus.CREATED).body(user);
+        } catch (EntityExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+        }
     }
 
     @GetMapping("/{userId}")
@@ -33,10 +53,10 @@ public class UserController {
         }
     }
 
-    @PostMapping("/add")
-    public User addUser(@RequestBody UserAndPasswordDTO userAndPasswordDTO) {
-        return userService.register(userAndPasswordDTO.getName(), userAndPasswordDTO.getPassword());
-    }
+//    @GetMapping("/all")
+//    public List<User> getAllUsers() {
+//        return userService.getAllUsers();
+//    }
 
     @PutMapping("/update/{userId}")
     public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody User updatedUser) {
@@ -58,20 +78,20 @@ public class UserController {
         }
     }
 
-    @PostMapping("/authenticate")
-    public ResponseEntity<Boolean> authenticateUser(@RequestBody UserAndPasswordDTO userAndPasswordDTO) {
-        System.out.println("Authenticating user: " + userAndPasswordDTO.getName()); // Print the username being authenticated
-        System.out.println("Password passed: : " + userAndPasswordDTO.getPassword()); // Print the password being authenticated
-        try {
-            User user = userService.findByName(userAndPasswordDTO.getName())
-                    .orElseThrow(() -> new IllegalArgumentException("User not found"));
-            boolean isAuthenticated = userService.authenticateUser(user, userAndPasswordDTO.getPassword());
-            System.out.println("Authentication result for " + userAndPasswordDTO.getName() + ": " + isAuthenticated); // Print the result of authentication
-
-            return new ResponseEntity<>(isAuthenticated, HttpStatus.OK);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error during authentication: " + e.getMessage()); // Print the error message
-            return new ResponseEntity<>(false, HttpStatus.UNAUTHORIZED); // Return an appropriate status code for unauthorized access
-        }
-    }
+//    @PostMapping("/authenticate")
+//    public ResponseEntity<Boolean> authenticateUser(@RequestBody UserAndPasswordDTO userAndPasswordDTO) {
+//        System.out.println("Authenticating user: " + userAndPasswordDTO.getName()); // Print the username being authenticated
+//        System.out.println("Password passed: : " + userAndPasswordDTO.getPassword()); // Print the password being authenticated
+//        try {
+//            User user = userService.findByName(userAndPasswordDTO.getName())
+//                    .orElseThrow(() -> new IllegalArgumentException("User not found"));
+//            boolean isAuthenticated = userService.authenticateUser(user, userAndPasswordDTO.getPassword());
+//            System.out.println("Authentication result for " + userAndPasswordDTO.getName() + ": " + isAuthenticated); // Print the result of authentication
+//
+//            return new ResponseEntity<>(isAuthenticated, HttpStatus.OK);
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("Error during authentication: " + e.getMessage()); // Print the error message
+//            return new ResponseEntity<>(false, HttpStatus.UNAUTHORIZED); // Return an appropriate status code for unauthorized access
+//        }
+//    }
 }
