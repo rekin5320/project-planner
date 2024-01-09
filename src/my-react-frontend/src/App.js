bc6effc01d270dafb1f3f33e71f61a200db44ee2
Jakub <01178575@pw.edu.pl>
2024-01-09 18:46:10 +0100

    Added GoogleAuthProvider

--- src/my-react-frontend/src/App.js
+++ src/my-react-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google'; // Import the GoogleOAuthProvider
 import Login from './components/Login';
 import Register from './components/Register';
 import HomePage from './components/HomePage';
@@ -25,12 +26,14 @@ function App() {
 
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<Navigate to="/login" />} />
-                <Route path="/login" element={<Login onLogin={handleLogin} />} />
-                <Route path="/register" element={<Register onRegister={handleRegister} />} />
-                <Route path="/home" element={isLoggedIn ? <HomePage user={user} onLogout={handleLogout} /> : <Navigate to="/login" />} />
-            </Routes>
+            <GoogleOAuthProvider clientId="653829545632-s1tg9di96ernst657soqhvtdt37vssp8.apps.googleusercontent.com">
+                <Routes>
+                    <Route path="/" element={<Navigate to="/login" />} />
+                    <Route path="/login" element={<Login onLogin={handleLogin} />} />
+                    <Route path="/register" element={<Register onRegister={handleRegister} />} />
+                    <Route path="/home" element={isLoggedIn ? <HomePage user={user} onLogout={handleLogout} /> : <Navigate to="/login" />} />
+                </Routes>
+            </GoogleOAuthProvider>
         </Router>
     );
 }
