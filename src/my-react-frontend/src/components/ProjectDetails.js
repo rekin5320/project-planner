f299252c8a26adfbefca63ed484d2132d4b14b32
Tomeczek <trunio31@gmail.com>
2024-01-10 13:02:02 +0100

    Added a go back button on ProjectDetails.js

--- src/my-react-frontend/src/components/ProjectDetails.js
+++ src/my-react-frontend/src/components/ProjectDetails.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ProjectDetails = ({ project }) => {
+
+    const navigate = useNavigate();
+
     if (!project) {
         return <div className="text-center text-lg text-gray-600">No project selected</div>;
     }
+
+    const goBack = () => {
+        navigate('/home');
+    };
 
     return (
         <div className="min-h-screen bg-gray-100 p-6 bg-custom-background" >
@@ -20,6 +28,11 @@ const ProjectDetails = ({ project }) => {
                     <p className="text-md text-gray-700">Owner: {project.owner && project.owner.name}</p>
                     {/* Add more fields as necessary */}
                 </div>
+            </div>
+            <div className="flex justify-center mt-4">
+                <button onClick={goBack} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Go Back to Home
+                </button>
             </div>
         </div>
     );
